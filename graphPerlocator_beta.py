d8b72fa24fc3c53e2f2689da20767556663de513
guido.biosca <guido.biosca@estudiantat.upc.edu>
2024-10-20 13:24:33 +0200

    proj

---
+++ graphPerlocator_beta.py
@@ -0,0 +1,172 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import random
+import numpy as np
+import multiprocessing
+import pandas as pd
+from datetime import datetime
+import itertools
+from pathlib import Path
+import configparser
+
+AVG_FRIENDS = 5 # Nombre mitjà de amics en un grup
+
+def percolate_graph(G, p, perl_vertex=True):
+    H = G.copy()
+    if not perl_vertex:
+        for edge in list(H.edges):
+            if random.random() > p:
+                H.remove_edge(*edge)
+    else:
+        for node in list(H.nodes):
+            if random.random() > p:
+                H.remove_node(node)
+    return nx.number_connected_components(H)
+
+def calculate_mitj(G, k, p, perl_vertex):
+    mitj = 0
+    for i in range(k):
+        mitj += percolate_graph(G, p, perl_vertex)
+    mitj = mitj/k
+    print(p,mitj)
+    return mitj, p
+
+def evaluate(G, k, q, perl_vertex=True):
+    p_values = np.linspace(0, 1, q)
+    args_list = [(G, k, p, perl_vertex) for p in p_values]
+    connected_fractions = []
+    with multiprocessing.Pool() as pool:
+        connected_fractions = pool.starmap(calculate_mitj, args_list)
+    return connected_fractions
+
+
+def plot_and_save(mediaandprob, graph_type, n, k, q, perl_vertex, show_plot=True):
+    media, prob = zip(*mediaandprob)
+    plt.figure(figsize=(10, 6))
+    plt.plot(prob, media, marker='o', linestyle='-', color='b')
+    title = 'Transición de Fase ' + graph_type
+    plt.title(title)
+    plt.xlabel('Probabilidad de Percolación (p)')
+    plt.ylabel('Media Grafos Conexos')
+    plt.legend()
+    plt.grid()
+
+    main_folder = Path('data')
+    folder_name = '{}_n{}_k{}_q{}_pvert{}'.format(graph_type, n, k, q, perl_vertex)
+    folder = main_folder / graph_type / folder_name
+    main_folder.mkdir(parents=True, exist_ok=True)
+    folder.mkdir(parents=True, exist_ok=True)
+
+    date = datetime.now()
+    date = date.strftime("%Y-%m-%d_%H:%M")
+    file_name = str(date) + '.csv'
+    df = pd.DataFrame({
+        'prob': prob,
+        'media': media
+    })
+    df.to_csv(str(folder) + '/' + file_name, index=False)
+    plot_name = str(date) + '.png'
+    plt.savefig(str(folder) + '/' + plot_name)
+    if show_plot:
+        plt.show()
+    else:
+        plt.close()
+
+def generate_connected_rgg(n, radius):
+    G = None
+    while G is None or not nx.is_connected(G):
+        G = nx.random_geometric_graph(n, radius)
+    return G
+
+def generate_connected_grid(n):
+    G = None
+    while G is None or not nx.is_connected(G):
+        G = nx.grid_2d_graph(n, n)
+    return G
+
+def generate_connected_3dgrid(n):
+    G = None
+    while G is None or not nx.is_connected(G):
+        G = nx.Graph()
+        for x, y, z in itertools.product(range(n), range(n), range(n)):
+            G.add_node((x, y, z))
+        for x, y, z in itertools.product(range(n), range(n), range(n)):
+            if x + 1 < n:
+                G.add_edge((x, y, z), (x + 1, y, z))
+            if y + 1 < n:
+                G.add_edge((x, y, z), (x, y + 1, z))
+            if z + 1 < n:
+                G.add_edge((x, y, z), (x, y, z + 1))
+    return G
+
+def generate_connected_caveman_graph(l, k):
+    G = nx.connected_caveman_graph(l, k)
+    return G
+
+def input_data():
+    graph_type = input('Introdueix tipus de graf (g2d, rgg, g3d, ccg): ')
+    if graph_type == 'g3d':
+        print('ALERTA: Consumeix molta memoria (posar n < 100)')
+    if graph_type != 'ccg':
+        n = int(input('Introdueix la mida del graf n{}: '.format(
+            'xn' if graph_type == 'g2d' else 'xnxn' if graph_type == 'g3d' else '')))
+    elif graph_type == 'ccg':
+        n = int(input('Introdueix el nombre de coves: '))
+    k = int(input('Nombre de proves per probabilitat: '))
+    q = int(input('Quants valors de probabilitat vols estudiar: '))
+    perl_vertex = False
+    if graph_type != 'rgg':
+        perl_vertex = int(input('Perlocar vertexs(1) o arestes(0)? '))
+    return graph_type, n, k, q, perl_vertex
+
+
+def generate_percolate_save(graph_type, n, k, q, perl_vertex,show_plot=True):
+    mediaandprob = None
+    if graph_type == 'g2d' or graph_type == 'g3d':
+        G = generate_connected_grid(n) if graph_type == 'g2d' else generate_connected_3dgrid(n)
+        mediaandprob = evaluate(G, k, q, perl_vertex)
+
+    elif graph_type == 'rgg':
+        radius = np.sqrt(np.log(n) / (np.pi * n)) #radi threshold per a la connexitud del RGG, si no surt connex es torna a generar
+        G = generate_connected_rgg(n, radius)
+        mediaandprob = evaluate(G, k, q, perl_vertex=False)
+    
+    elif graph_type == 'ccg':
+        G = generate_connected_caveman_graph(n, AVG_FRIENDS)
+        mediaandprob =evaluate(G, k, q, perl_vertex)
+
+    plot_and_save(mediaandprob, graph_type, n, k, q, perl_vertex, show_plot)
+
+def parameters_reader():
+    config = configparser.ConfigParser()
+    config_name = input('config file name: ')
+    config.read(config_name)
+    lista = config.get('config', 'n').split(',')
+    n = [int(i) for i in lista]
+    k = config.getint('config', 'k')
+    q = config.getint('config', 'q')
+    perl_vertex = config.getint('config', 'perl_vertex')
+    graph_type = config.get('config', 'graph_type')
+    return n, k, q, perl_vertex, graph_type
+
+
+def main():
+    automatic_generation = input('Generacio automatica o manual (a, m): ')
+    if automatic_generation == 'm':
+        graph_type, n, k, q, perl_vertex = input_data()
+        generate_percolate_save(graph_type, n, k, q, perl_vertex)
+    else:
+        n, k, q, perl_vertex, graph_type = parameters_reader()
+        for n_in in n:
+            print('VALOR ', n_in, '////////')
+            generate_percolate_save(graph_type, n_in, k, q, perl_vertex, False)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
---
+++ plotter.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from pathlib import Path
+import os
+import numpy as np
+
+
+def load_data(graph_type=None, n_range=None, k_range=None, q_range=None, perl_vertex=None):
+    # Definir el directorio base
+    base_dir = Path('data')
+
+    # Inicializar lista para almacenar los DataFrames
+    all_data = []
+
+    # Recorrer las subcarpetas y cargar los CSVs
+    for root, dirs, files in os.walk(base_dir):
+        for file in files:
+            if file.endswith('.csv'):
+                file_path = os.path.join(root, file)
+                folder = Path(root)
+                folder_parts = folder.parts
+
+                # Verificar si el nombre de la carpeta tiene el formato esperado
+                try:
+                    graph_type_folder = folder_parts[-2]
+                    params = folder_parts[-1].split('_')
+
+                    # Verificar que los valores sean convertibles a enteros
+                    graph_type_curr = graph_type_folder
+                    n_curr = int(params[1][1:]) if params[1][1:].isdigit() else None
+                    k_curr = int(params[2][1:]) if params[2][1:].isdigit() else None
+                    q_curr = int(params[3][1:]) if params[3][1:].isdigit() else None
+                    perl_vertex_curr = int(params[4][5]) if params[4][5].isdigit() else None
+
+                    # Filtrar según los parámetros proporcionados, ignorando los que sean None
+                    if (graph_type is None or graph_type == graph_type_curr) and \
+                            (n_range is None or (n_curr is not None and n_range[0] <= n_curr <= n_range[1] and n_curr%5 == 0)) and \
+                            (k_range is None or (k_curr is not None and k_range[0] <= k_curr <= k_range[1])) and \
+                            (q_range is None or (q_curr is not None and q_range[0] <= q_curr <= q_range[1])) and \
+                            (perl_vertex is None or (perl_vertex_curr is not None and perl_vertex == perl_vertex_curr)):
+                        # Cargar el CSV
+                        df = pd.read_csv(file_path)
+                        df['graph_type'] = graph_type_curr
+                        df['n'] = n_curr
+                        df['k'] = k_curr
+                        df['q'] = q_curr
+                        df['perl_vertex'] = perl_vertex_curr
+                        all_data.append(df)
+                except Exception as e:
+                    print(f"Error al procesar {file_path}: {e}")
+
+    # Combinar todos los DataFrames
+    if all_data:
+        return pd.concat(all_data, ignore_index=True)
+    else:
+        return pd.DataFrame()  # Retornar DataFrame vacío si no se cargan datos
+
+
+def plot_data(df):
+    if df.empty:
+        print("No se encontraron datos con los filtros seleccionados.")
+        return
+
+    # Agrupar los datos por valor de 'n' y calcular el valor máximo de cada curva
+    n_values = sorted(df['n'].unique())
+    max_values = {}
+
+    for n in n_values:
+        df_n = df[df['n'] == n]
+        grouped = df_n.groupby('prob')['media'].mean()
+        max_values[n] = grouped.max()  # Guardamos el valor máximo de cada curva
+
+    # Ordenar las curvas en función de su valor máximo
+    sorted_n_values = sorted(max_values, key=max_values.get)
+
+    plt.figure(figsize=(10, 6))
+    colors = plt.cm.viridis(np.linspace(0, 1, len(sorted_n_values)))
+
+    lower_bound = np.zeros_like(df.groupby('prob')['media'].mean().values)  # Iniciar límite inferior en 0
+
+    # Graficar en el orden de los valores máximos
+    for i, n in enumerate(sorted_n_values):
+        df_n = df[df['n'] == n]
+        grouped = df_n.groupby('prob')['media'].mean()
+
+        # Graficar la línea
+        plt.plot(grouped.index, grouped.values, linestyle='-', color=colors[i], label=f'n={n}')
+
+        # Colorear entre la curva actual y la curva anterior (lower_bound)
+        plt.fill_between(grouped.index, lower_bound, grouped.values, color=colors[i], alpha=0.7)
+
+        # Actualizar el límite inferior para la siguiente curva
+        lower_bound = grouped.values
+
+    plt.title('Número medio de componentes conexas para diferentes valores de N')
+    #plt.title('Número medio de componentes conexas para N = 100')
+    plt.xlabel('Probabilidad de percolación (p)')
+    plt.ylabel('Media de componentes conexas')
+    plt.legend(title='Valor de n')
+    plt.grid(True)
+    plot_name =  'plot.png'
+    plt.savefig(plot_name)
+    #plt.xlim(0, 0.2)
+
+    plt.show()
+
+# Ejemplo de uso:
+# Filtrar por grafos de tipo 'g2d' y n entre 10 y 40
+graph_type = input('graph type (g2d, rgg, g3d, ccg): ')
+n_range_a = int(input('n_range a: '))
+n_range_b = int(input('n_range b: '))
+prob = int(input('number of prob: '))
+inst_per_prob = int(input('intancies per prob: '))
+perl_vertex = int(input('perlocar vertex (1 o 0): '))
+data = load_data(graph_type=graph_type, n_range=(n_range_a, n_range_b), k_range=(inst_per_prob, inst_per_prob),q_range=(prob, prob),perl_vertex=perl_vertex)
+plot_data(data)
